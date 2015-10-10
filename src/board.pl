352f8da549e88030c0557afee38993b1e72f6e06
Teresa Chaves <up201306842@fe.up.pt>
2015-10-10 22:38:12 +0100

    relatorio update and src update

--- src/board.pl
+++ src/board.pl
@@ -1,33 +1,74 @@
+/*
 
+TRANSLATE - convert a number to figure (_, O or X)
+The first parameter is a possible board cell number.
+The second parameter is the respective translation to a figure.
 
+*/
 
+translate(0, '_').
+translate(1, 'O').
+translate(2, 'X').
 
+/*
+
+MAKE_LINE - make a game board line
+Size - The size of the line
+Line - Line to be returned
+
+*/
 
 make_line(0, []).
 make_line(Size, Line):- NewSize is Size-1,
                         make_line(NewSize, NewLine), 
                         append(NewLine, [0], Line).
 
+/*
+
+PRINT_LINE - print a line of the board
+H - The H (head) is a board cell represented by one of the following numbers: 0, 1 or 2
+T - The T (tail) is the others cells of that line
+*/
+                                         
+print_line([]):- write(' |').
+
+print_line([H|T]):- write(' | '),
+                    translate(H, C), %convert number to figure (_, O or X)
+                    write(C),
+                    print_line(T).
+                                                
+/*
+
+PRINT_BOARD- print a size NxN board
+H - The H (head) is a line
+T - The T (tail) is the other lines
+
+*/
+                                                
 print_board([]).
 print_board([H|T]):- print_line(H), nl,
                      print_board(T).
 
-print_line([]):- write('|').
-print_line([H|T]):- write('| _ '),
-                    write(H),
-                    write(' _ '),
-                    print_line(T).
+/*
 
-make_board(Size, Board):- make_board(Size, 0, Board).
+MAKE_BOARD - make a board of size NxN
+Size - Board's size
+Counter - Size's counter (starts in 0 and ends when Size = Counter)
+Board - Game board of size NxN
+*/
+                                        %Using cut (!) to ensure SICStus doesn't give more solutions
+make_board(Size, Board):- make_board(Size, 0, Board), !.
+                
+make_board(S, S, []). %When Size = Counter
 
-make_board(S, S, []).
-
-make_board(Size, Counter, Board):- NewCounter is Counter + 1,
+make_board(Size, Counter, Board):- Size > Counter,
+                                   NewCounter is Counter + 1,
                                    make_line(Size, Line),
                                    make_board(Size, NewCounter, NewBoard),
-                                   append(NewBoard, Line, Board).
+                                   append(NewBoard, [Line], Board). 
+                                                        %append the Line to NewBoard, thus creating the final Board
 
-        
+
 /*
    | _   _ | _ x _ | _   _ | _ o _ | _ o _ |
    | _ o _ | _   _ | _ x _ | _ x _ | _ x _ |
@@ -39,4 +80,4 @@ make_board(Size, Counter, Board):- NewCounter is Counter + 1,
    _ - 0
    o - 1
    x - 2
-   */
+*/
