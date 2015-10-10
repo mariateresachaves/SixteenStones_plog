a4db7b3602abc89a8a33599257bd8107cf69dc14
Teresa Chaves <up201306842@fe.up.pt>
2015-10-10 09:01:18 +0100

    make_line, print_board, print_line

---
+++ src/board.pl
@@ -0,0 +1,29 @@
+make_line(0, []).
+make_line(Size, Line):- NewSize is Size-1,
+                        make_line(NewSize, NewLine), 
+                        append(NewLine, [0], Line).
+
+print_board([]).
+print_board([H|T]):- print_line(H), nl,
+                     print_board(T).
+
+print_line([]):- write('|').
+print_line([H|T]):- write('| _ '),
+                    write(H),
+                    write(' _ '),
+                    print_line(T).
+
+
+        
+/*
+   | _   _ | _ x _ | _   _ | _ o _ | _ o _ |
+   | _ o _ | _   _ | _ x _ | _ x _ | _ x _ |
+   | _ x _ | _   _ | _   _ | _ o _ | _ x _ |
+   | _   _ | _   _ | _ o _ | _ o _ | _   _ |
+   | _ x _ | _ x _ | _ o _ | _ o _ | _   _ |
+    
+   
+   _ - 0
+   o - 1
+   x - 2
+   */
